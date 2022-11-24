92afd07ef28d79902cf8e2c436dbeda2cd660fb1
jrleijnse <jr_leijnse@live.nl>
2022-11-24 21:50:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const icons = document.querySelectorAll(".icon");
+
+for (let icon of icons) {
+  const item = icon.parentElement;
+
+  icon.addEventListener("click", function () {
+    item.classList.toggle("open");
+  });
+}
+
+console.log("hi");
+console.log(icons);
